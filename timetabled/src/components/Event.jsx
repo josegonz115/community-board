52a3e422f69f41ba2cd395f30ed992a2eecedc73
Tanush Goel <tanushgoel03@gmail.com>
2023-09-16 12:00:24 -0700

    Table

---
+++ timetabled/src/components/Event.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Event = (props) => {
+  return (<div>
+    <td className= "Event">
+        <h5> Test Event Name</h5>
+    </td>
+  </div>)
+}
+
+export default Event;
